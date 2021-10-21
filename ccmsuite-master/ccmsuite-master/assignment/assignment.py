d42efb52e0aa56aff6833d3814062babea5cca84
luukthekeeper <luuk2luuk@gmail.com>
2021-10-21 11:51:52 +0200

    Added Final Comments

--- ccmsuite-master/ccmsuite-master/assignment/assignment.py
+++ ccmsuite-master/ccmsuite-master/assignment/assignment.py
@@ -3,22 +3,28 @@ from ccm.lib.actr import *
 log=ccm.log(html=True)
 import random
 
-#1 = 123
-#2 = 12
-#3 = 23
-#4 = 1
-#5 =2
-#6 = 3
-#7 = empty
-#8 = 13
+## All possible states a peg could take
+## Disk 1 is the biggest disk, disk 3 is the smallest and disk 2 is in between
+## Disks can only be moved to a peg if the top disk of this peg is larger than the disk to be moved or if this peg has no disks
+
+#1 = 123    i.e. this peg has all disks, the order of how the disks are placed from bottom to top should be read from left to right
+#2 = 12     similar to state 1 except no disk 3   
+#3 = 23     similar to state 1 except no disk 1
+#4 = 1      only disk 1
+#5 = 2      only disk 2
+#6 = 3      only disk 3
+#7 = empty  no disks
+#8 = 13     similar to state 1 except no disk 2
 
 
 class model(ACTR):
     goal=Buffer()
-    #123           e               e
+    
+    #Above every function, the state is visualized for which that function will be applied
+    #123           empty               empty                                      
     def move_1(goal='action:move pegA:1 pegB:7 pegC:7'):
-        num = random.randint(0,1)
-        if num ==0:
+        num = random.randint(0,1)          #initializing a random number between the number of possible moves
+        if num == 0:                       #in order to pick a random move
             print("Disk  3 was moved to peg B")
             print("Peg A has disks [1,2], peg B has disks [3], peg C has disks []")
             goal.modify(pegA='2', pegB='6')
@@ -27,14 +33,14 @@ class model(ACTR):
             print("Peg A has disks [1,2], peg B has disks [], peg C has disks [3]")
             goal.modify(pegA='2', pegC='6')
 
-    #12            e               3 
+    #12            empty              3 
     def move_2(goal='action:move pegA:2 pegB:7 pegC:6'):
         num = random.randint(0,2)
         if num == 0:
             print("Disk 2 was moved to peg B")
             print("Peg A has disks [1], peg B has disks [2], peg C has disks [3]")
             goal.modify(pegA='4', pegB= '5')
-        elif num ==1:
+        elif num == 1:
             print("Disk 3 has moved to peg B")
             print("Peg A has disks [1,2], peg B has disks [3], peg C has disks []")
             goal.modify(pegB='6', pegC='7')
@@ -46,11 +52,11 @@ class model(ACTR):
     #1              2               3
     def move_3(goal='action:move pegA:4 pegB:5 pegC:6'):
         num = random.randint(0,2)
-        if num ==0:
+        if num == 0:
             print("Disk 3 was moved to peg B")
             print("Peg A has disks [1], peg B has disks [2,3], peg C has disks []")
             goal.modify(pegB ='3', pegC='7')
-        elif num ==1:
+        elif num == 1:
             print("Disk 3 was moved to peg A")
             print("Peg A has disks [1,3], peg B has disks [2], peg C has disks []")
             goal.modify(pegA ='8', pegC='7')
@@ -59,14 +65,14 @@ class model(ACTR):
             print("Peg A has disks [1,2], peg B has disks [], peg C has disks [3]")
             goal.modify(pegA='2', pegB='7')
         
-    #1              23              e
+    #1              23              empty
     def move_4(goal='action:move pegA:4 pegB:3 pegC:7'):
         num = random.randint(0,2)
-        if num ==0:
+        if num == 0:
             print("Disk 1 was moved to peg C")
             print("Peg A has disks [], peg B has disks [2,3], peg C has disks [1]")
             goal.modify(pegA='7', pegC='4')
-        elif num ==1:
+        elif num == 1:
             print("Disk 3 was moved to peg A")
             print("Peg A has disks [1,3], peg B has disks [2], peg C has disks []")
             print("moet nog gedaan worden")
@@ -76,7 +82,7 @@ class model(ACTR):
             print("Peg A has disks [1], peg B has disks [2], peg C has disks [3]")
             goal.modify(pegB='5', pegC='6')
 
-    #e              23              1
+    #empty              23              1
     def move_5(goal='action:move pegA:7 pegB:3 pegC:4'):
         num = random.randint(0,2)
         if num == 0:
@@ -95,7 +101,7 @@ class model(ACTR):
     #3              2               1
     def move_6(goal='action:move pegA:6 pegB:5 pegC:4'):
         num = random.randint(0,2)
-        if num ==0:
+        if num == 0:
             print("Disk 2 was moved to peg C")
             print("Peg A has disks [3], peg B has disks [], peg C has disks [1,2]")
             goal.modify(pegB='7', pegC='2')
@@ -109,14 +115,14 @@ class model(ACTR):
             goal.modify(pegA='7', pegB='3')
 
 
-    #3              e              12
+    #3              empty              12
     def move_7(goal='action:move pegA:6 pegB:7 pegC:2'):
         num = random.randint(0,2)
-        if num ==0:
+        if num == 0:
             print("Disk 3 was moved to peg C")
             print("Peg A has disks [], peg B has disks [], peg C has disks [1,2,3]")
             goal.modify(pegA='7', pegC='1')
-        elif num ==1:
+        elif num == 1:
             print("Disk 2 was moved to peg B")
             print("Peg A has disks [3], peg B has disks [2], peg C has disks [1]")
             goal.modify(pegB='5', pegC='4')
@@ -125,14 +131,14 @@ class model(ACTR):
             print("Peg A has disks [], peg B has disks [3], peg C has disks [1,2]")          
             goal.modify(pegA='7', pegB='6')
 
-    #12             3               e
+    #12             3               empty
     def move_8(goal='action:move pegA:2 pegB:6 pegC:7'):
         num = random.randint(0,2)
-        if num ==0:
+        if num == 0:
             print("Disk 1 was moved to peg C")
             print("Peg A has disks [1,2], peg B has disks [], peg C has disks [3]")
             goal.modify(pegB='7', pegC='6')
-        elif num ==1:
+        elif num == 1:
             print("Disk 3 was moved to peg A")
             print("Peg A has disks [1,2,3], peg B has disks [], peg C has disks []")
             goal.modify(pegA='1', pegB='7')
@@ -141,14 +147,14 @@ class model(ACTR):
             print("Peg A has disks [1], peg B has disks [3], peg C has disks [2]")
             goal.modify(pegA='4', pegC='5')
 
-    #13             2               e
+    #13             2               empty
     def move_9(goal='action:move pegA:8 pegB:5 pegC:7'):
         num = random.randint(0,2)
-        if num ==0:
+        if num == 0:
             print("Disk 3 was moved to peg C")
             print("Peg A has disks [1], peg B has disks [2], peg C has disks [3]")
             goal.modify(pegA='4', pegC='6')
-        elif num ==1:
+        elif num == 1:
             print("Disk 3 was moved to peg B")
             print("Peg A has disks [1], peg B has disks [2,3], peg C has disks []")
             goal.modify(pegA='4', pegB='3')
@@ -157,14 +163,14 @@ class model(ACTR):
             print("Peg A has disks [1,3], peg B has disks [], peg C has disks [2]")
             goal.modify(pegB='7', pegC='5')
 
-    #e              2               13
+    #empty              2               13
     def move_10(goal='action:move pegA:7 pegB:5 pegC:8'):
         num = random.randint(0,2)
-        if num ==0:
+        if num == 0:
             print("Disk 2 was moved to peg A")
             print("Peg A has disks [2], peg B has disks [], peg C has disks [1,3]")
             goal.modify(pegA='5', pegB='7')
-        elif num==1:
+        elif num == 1:
             print("Disk 3 was moved to peg A")
             print("Peg A has disks [3], peg B has disks [2], peg C has disks [1]")
             goal.modify(pegA='4', pegC='6')
@@ -173,14 +179,14 @@ class model(ACTR):
             print("Peg A has disks [], peg B has disks [2,3], peg C has disks [1]")
             goal.modify(pegB='3', pegC='4')
 
-    #e              3               12
+    #empty              3               12
     def move_11(goal='action:move pegA:7 pegB:6 pegC:2'):
         num = random.randint(0,2)
-        if num ==0:
+        if num == 0:
             print("Disk  3 was moved to peg A")
             print("Peg A has disks [3], peg B has disks [], peg C has disks [1,2]")
             goal.modify(pegA='6', pegB='7')
-        elif num==1:
+        elif num == 1:
             print("Disk 3 was moved to peg C")
             print("Peg A has disks [], peg B has disks [], peg C has disks [1,2,3]")
             goal.modify(pegB='7', pegC='1')
@@ -192,11 +198,11 @@ class model(ACTR):
     #1          3           2
     def move_12(goal='action:move pegA:4 pegB:6 pegC:5'):
         num = random.randint(0,2)
-        if num ==0:
+        if num == 0:
             print("Disk 3 was moved to peg A")
             print("Peg A has disks [1,3], peg B has disks [], peg C has disks [2]")
             goal.modify(pegA='8', pegB='7')
-        elif num==1:
+        elif num == 1:
             print("Disk 3 was moved to peg C")
             print("Peg A has disks [1], peg B has disks [], peg C has disks [2,3]")
             goal.modify(pegB='7', pegC='3')
@@ -205,14 +211,14 @@ class model(ACTR):
             print("Peg A has disks [1,2], peg B has disks [3], peg C has disks []")
             goal.modify(pegA='2', pegC='7')
 
-    #13            e            2
+    #13            empty            2
     def move_13(goal='action:move pegA:8 pegB:7 pegC:5'):
         num = random.randint(0,2)
-        if num ==0:
+        if num == 0:
             print("Disk  2 was moved to peg B")
             print("Peg A has disks [1,3], peg B has disks [2], peg C has disks []")
             goal.modify(pegB='5', pegC='7')
-        elif num==1:
+        elif num == 1:
             print("Disk 3 was moved to peg C")
             print("Peg A has disks [1], peg B has disks [], peg C has disks [2,3]")
             goal.modify(pegA='4', pegC='3')
@@ -221,14 +227,14 @@ class model(ACTR):
             print("Peg A has disks [1], peg B has disks [3], peg C has disks [2]")
             goal.modify(pegA='4', pegB='6')
 
-    #2          e           13
+    #2          empty           13
     def move_14(goal='action:move pegA:5 pegB:7 pegC:8'):
         num = random.randint(0,2)
-        if num ==0:
+        if num == 0:
             print("Disk  2 was moved to peg B")
             print("Peg A has disks [], peg B has disks [2], peg C has disks [1,3]")
             goal.modify(pegA='7', pegB='5')
-        elif num==1:
+        elif num == 1:
             print("Disk 3 was moved to peg A")
             print("Peg A has disks [2,3], peg B has disks [], peg C has disks [1]")
             goal.modify(pegA='3', pegC='4')
@@ -240,11 +246,11 @@ class model(ACTR):
     #2          3           1
     def move_15(goal='action:move pegA:5 pegB:6 pegC:4'):
         num = random.randint(0,2)
-        if num ==0:
+        if num == 0:
             print("Disk  3 was moved to peg A")
             print("Peg A has disks [2,3], peg B has disks [], peg C has disks [1]")
             goal.modify(pegA='3', pegB='7')
-        elif num==1:
+        elif num == 1:
             print("Disk 3 was moved to peg C")
             print("Peg A has disks [2], peg B has disks [], peg C has disks [1,3]")
             goal.modify(pegB='7', pegC='8')
@@ -253,14 +259,14 @@ class model(ACTR):
             print("Peg A has disks [], peg B has disks [3], peg C has disks [1,2]")
             goal.modify(pegA='7', pegC='2')
 
-    #1          e           23
+    #1          empty           23
     def move_16(goal='action:move pegA:4 pegB:7 pegC:3'):
         num = random.randint(0,2)
-        if num ==0:
+        if num == 0:
             print("Disk  3 was moved to peg A")
             print("Peg A has disks [1,3], peg B has disks [], peg C has disks [2]")
             goal.modify(pegA='8', pegC='5')
-        elif num==1:
+        elif num == 1:
             print("Disk 3 was moved to peg B")
             print("Peg A has disks [1], peg B has disks [3], peg C has disks [2]")
             goal.modify(pegB='6', pegC='5')
@@ -269,14 +275,14 @@ class model(ACTR):
             print("Peg A has disks [], peg B has disks [1], peg C has disks [2,3]")
             goal.modify(pegA='7', pegB='4')
 
-    #23         e           1
+    #23         empty           1
     def move_17(goal='action:move pegA:3 pegB:7 pegC:4'):
         num = random.randint(0,2)
-        if num ==0:
+        if num == 0:
             print("Disk  3 was moved to peg B")
             print("Peg A has disks [2], peg B has disks [3], peg C has disks [1]")
             goal.modify(pegA='5', pegB='6')
-        elif num==1:
+        elif num == 1:
             print("Disk 3 was moved to peg C")
             print("Peg A has disks [2], peg B has disks [], peg C has disks [1,3]")
             goal.modify(pegA='5', pegC='8')
@@ -285,14 +291,14 @@ class model(ACTR):
             print("Peg A has disks [2,3], peg B has disks [1], peg C has disks []")
             goal.modify(pegB='4', pegC='7')
     
-    #e          1               23
+    #empty          1               23
     def move_18(goal='action:move pegA:7 pegB:4 pegC:3'):
         num = random.randint(0,2)
-        if num ==0:
+        if num == 0:
             print("Disk  1 was moved to peg A")
             print("Peg A has disks [1], peg B has disks [], peg C has disks [2,3]")
             goal.modify(pegA='4', pegB='7')
-        elif num==1:
+        elif num == 1:
             print("Disk 3 was moved to peg A")
             print("Peg A has disks [3], peg B has disks [1], peg C has disks [2]")
             goal.modify(pegA='6', pegC='5')
@@ -301,14 +307,14 @@ class model(ACTR):
             print("Peg A has disks [], peg B has disks [1,3], peg C has disks [2]")
             goal.modify(pegB='8', pegC='5')
 
-    #23         1               e
+    #23         1               empty
     def move_19(goal='action:move pegA:3 pegB:4 pegC:7'):
         num = random.randint(0,2)
-        if num ==0:
+        if num == 0:
             print("Disk  3 was moved to peg B")
             print("Peg A has disks [2], peg B has disks [1,3], peg C has disks []")
             goal.modify(pegA='5', pegB='8')
-        elif num==1:
+        elif num == 1:
             print("Disk 3 was moved to peg C")
             print("Peg A has disks [2], peg B has disks [1], peg C has disks [3]")
             goal.modify(pegA='5', pegC='6')
@@ -320,11 +326,11 @@ class model(ACTR):
     #3              1           2
     def move_20(goal='action:move pegA:6 pegB:4 pegC:5'):
         num = random.randint(0,2)
-        if num ==0:
+        if num == 0:
             print("Disk  3 was moved to peg B")
             print("Peg A has disks [], peg B has disks [1,3], peg C has disks [2]")
             goal.modify(pegA='7', pegB='8')
-        elif num==1:
+        elif num == 1:
             print("Disk 3 was moved to peg C")
             print("Peg A has disks [], peg B has disks [1], peg C has disks [2,3]")
             goal.modify(pegA='7', pegC='3')
@@ -334,14 +340,14 @@ class model(ACTR):
             goal.modify(pegB='2', pegC='7')
 
 
-    #e              13              2
+    #empty              13              2
     def move_21(goal='action:move pegA:7 pegB:8 pegC:5'):
         num = random.randint(0,2)
-        if num ==0:
+        if num == 0:
             print("Disk  3 was moved to peg A")
             print("Peg A has disks [3], peg B has disks [1], peg C has disks [2]")
             goal.modify(pegA='6', pegB='4')
-        elif num==1:
+        elif num == 1:
             print("Disk 3 was moved to peg C")
             print("Peg A has disks [], peg B has disks [1], peg C has disks [2,3]")
             goal.modify(pegB='4', pegC='3')
@@ -350,14 +356,14 @@ class model(ACTR):
             print("Peg A has disks [2], peg B has disks [1,3], peg C has disks []")
             goal.modify(pegA='5', pegC='7')
 
-    #2              13              e
+    #2              13              empty
     def move_22(goal='action:move pegA:5 pegB:8 pegC:7'):
         num = random.randint(0,2)
-        if num ==0:
+        if num == 0:
             print("Disk  3 was moved to peg A")
             print("Peg A has disks [2,3], peg B has disks [1], peg C has disks []")
             goal.modify(pegA='3', pegB='4')
-        elif num==1:
+        elif num == 1:
             print("Disk 3 was moved to peg C")
             print("Peg A has disks [2], peg B has disks [1], peg C has disks [3]")
             goal.modify(pegB='4', pegC='6')
@@ -369,11 +375,11 @@ class model(ACTR):
     #2          1           3
     def move_23(goal='action:move pegA:5 pegB:4 pegC:6'):
         num = random.randint(0,2)
-        if num ==0:
+        if num == 0:
             print("Disk  3 was moved to peg A")
             print("Peg A has disks [2,3], peg B has disks [1], peg C has disks []")
             goal.modify(pegA='3', pegC='7')
-        elif num==1:
+        elif num == 1:
             print("Disk 3 was moved to peg B")
             print("Peg A has disks [2], peg B has disks [1,3], peg C has disks []")
             goal.modify(pegB='8', pegC='7')
@@ -382,14 +388,14 @@ class model(ACTR):
             print("Peg A has disks [], peg B has disks [1,2], peg C has disks [3]")
             goal.modify(pegA='7', pegB='2')
 
-        #3              12              e
+        #3              12              empty
     def move_24(goal='action:move pegA:6 pegB:2 pegC:7'):
         num = random.randint(0,2)
-        if num ==0:
+        if num == 0:
             print("Disk  3 was moved to peg C")
             print("Peg A has disks [], peg B has disks [1,2], peg C has disks [3]")
             goal.modify(pegA='7', pegC='6')
-        elif num==1:
+        elif num == 1:
             print("Disk 3 was moved to peg B")
             print("Peg A has disks [], peg B has disks [1,2,3], peg C has disks []")
             goal.modify(pegA='7', pegB='1')
@@ -398,14 +404,14 @@ class model(ACTR):
             print("Peg A has disks [3], peg B has disks [1], peg C has disks [2]")
             goal.modify(pegB='4', pegC='5')
 
-    #e          12              3
+    #empty          12              3
     def move_25(goal='action:move pegA:7 pegB:2 pegC:6'):
         num = random.randint(0,2)
-        if num ==0:
+        if num == 0:
             print("Disk  3 was moved to peg A")
             print("Peg A has disks [3], peg B has disks [1,2], peg C has disks []")
             goal.modify(pegA='6', pegC='7')
-        elif num==1:
+        elif num == 1:
             print("Disk 3 was moved to peg B")
             print("Peg A has disks [], peg B has disks [1,2,3], peg C has disks []")
             goal.modify(pegB='1', pegC='7')
@@ -414,10 +420,10 @@ class model(ACTR):
             print("Peg A has disks [2], peg B has disks [1], peg C has disks [3]")
             goal.modify(pegA='5', pegB='4')
 
-    #e                  123             e
+    #empty                  123             empty
     def move_26(goal='action:move pegA:7 pegB:1 pegC:7'):
         num = random.randint(0,1)
-        if num ==0:
+        if num == 0:
             print("Disk  3 was moved to peg A")
             print("Peg A has disks [3], peg B has disks [1,2], peg C has disks []")
             goal.modify(pegA='6', pegB='2')
@@ -426,7 +432,7 @@ class model(ACTR):
             print("Peg A has disks [], peg B has disks [1,2], peg C has disks [3]")
             goal.modify(pegB='2', pegC='6')
 
-    #e              e               123
+    #empty              empty               123
     def finish(goal='action:move pegC:1'):
         print("Problem solved")
         goal.clear()
